678c020ce2cb6ef9b6b1a094e8b2859bf22638c6
RenataCehajic <renata.cehajic@gmail.com>
2021-02-24 13:55:21 +0100

    Add serach button in Discover Movies

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function DiscoverMoviesPage() {
+  const [searchText, set_searchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
   return (
     <div>
-      <p>Discover Movies Page</p>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={(event) => set_searchText(event.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
     </div>
   );
 }
