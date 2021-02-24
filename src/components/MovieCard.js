fc91cd09c206d07818e18b477f6b4727513dc87a
RenataCehajic <renata.cehajic@gmail.com>
2021-02-24 19:43:33 +0100

    Add alt to image due to Netlify deployment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,7 +8,14 @@ function MovieCard({ title, year, poster, imdbId }) {
       <Link to={`/movie/${imdbId}`}>
         <h3 className="title">{title}</h3>
         <h4>{year}</h4>
-        <h4> {poster !== "N/A" ? <img src={poster} /> : <p>no image</p>}</h4>
+        <h4>
+          {" "}
+          {poster !== "N/A" ? (
+            <img src={poster} alt={poster} />
+          ) : (
+            <p>no image</p>
+          )}
+        </h4>
       </Link>
     </div>
   );
